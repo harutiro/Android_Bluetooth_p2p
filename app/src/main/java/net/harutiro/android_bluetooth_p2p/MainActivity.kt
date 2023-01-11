7b68891f7e48495b5a84260862e2a053db2f6c47
harutiro <harutiro2027@outlook.jp>
2023-01-11 23:36:17 +0900

    Initial commit

---
+++ app/src/main/java/net/harutiro/android_bluetooth_p2p/MainActivity.kt
@@ -0,0 +1,15 @@
+package net.harutiro.android_bluetooth_p2p
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+
+    }
+}
